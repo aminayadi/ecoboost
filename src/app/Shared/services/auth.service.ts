2c2cba3d437ce8dbc6e70c5f628feed0622eced5
= <=>
2022-11-18 05:51:49 +0100

    adding signup service + fix sign up beahaviour

--- src/app/Shared/services/auth.service.ts
+++ src/app/Shared/services/auth.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
 
 
-  isNotlogPage?: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  import { HttpClient, HttpHeaders } from '@angular/common/http';
+  import { BehaviorSubject, Observable } from 'rxjs';
+  import { Injectable } from '@angular/core';
 
-  constructor() {
 
+  
+  const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class AuthService {
+    constructor(private http: HttpClient) { }
+    isNotlogPage?: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+
+    login(username: string, password: string): Observable<any> {
+      return this.http.post('http://localhost:8080/api/authenticate', {
+        'username' : username,
+        'password' : password
+      }, httpOptions);
+    }
+  
+    register(username: string, email: string, password: string): Observable<any> {
+      return this.http.post('http://localhost:8080/api/register', {
+        'login' : username,
+        'email' : email,
+        'password' : password
+      }, httpOptions);
+    }
   }
-
-
-
-
-}
